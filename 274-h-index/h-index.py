c479ae2cf30d49807678dc5bcd931795d4a0f6a3
David Salas Carrascal <84999952+DaveTron4@users.noreply.github.com>
2026-01-25 13:51:32 -0500

    Time: 3 ms (26.21%) | Memory: 19.4 MB (22.56%) - LeetSync

--- 274-h-index/h-index.py
+++ 274-h-index/h-index.py
@@ -1,13 +1,34 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
-        citations.sort()
-        
+
         n = len(citations)
-        
-        for i in range(n):
-            count = n - i
-        
-            if citations[i] >= count:
-                return count
+
+        bucket = [0] * (n + 1)
+
+        for c in citations:
+            if c >= n:
+                bucket[n] += 1
+            else:
+                bucket[c] += 1
+
+        total_papers = 0
+        for i in range(n, -1, -1):
+            total_papers += bucket[i]
+
+            if total_papers >= i:
+                return i
 
         return 0
+
+        # citations.sort()
+        
+        # n = len(citations)
+        
+        # for i in range(n):
+        #     count = n - i
+        
+        #     if citations[i] >= count:
+        #         return count
+
+        # return 0
+
