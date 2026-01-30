e9445f20dc4892988a1943e0761103e0a3e6688c
David Salas Carrascal <84999952+DaveTron4@users.noreply.github.com>
2026-01-29 23:21:15 -0500

    Time: 15 ms (35.09%) | Memory: 19.4 MB (24.35%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.py
+++ 6-zigzag-conversion/zigzag-conversion.py
@@ -3,15 +3,15 @@ class Solution:
         if numRows == 1:
             return s
 
-        res = ""
+        result = ""
 
         for r in range(numRows):
             increment = 2 * (numRows - 1)
 
             for i in range(r, len(s), increment):
-                res += s[i]
+                result += s[i]
 
                 if (r > 0 and r < numRows - 1 and i + increment - 2 * r < len(s)):
-                    res += s[i + increment - 2 * r]
+                    result += s[i + increment - 2 * r]
 
-        return res
+        return result
