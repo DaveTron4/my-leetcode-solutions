15591722b6d79ea3742bb43b24b32c2116f7b607
David Salas Carrascal <84999952+DaveTron4@users.noreply.github.com>
2026-09-11 12:58:37 -0400

    Time: 15 ms (92.86%) | Memory: 12.4 MB (26.19%) - LeetSync

--- 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.py
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.py
@@ -4,34 +4,11 @@ class Solution(object):
         :type digits: List[int]
         :rtype: int
         """
-        hashmap = {}
-        index = 0
-        for i in digits:
-            hashmap[index] = i
-            index += 1
+        result = set()
 
-        print(hashmap)
-        total_unique = len(hashmap)
-
-
-        if set(digits) <= 2:
-            return 1
-
-        final = set()
-
-        for i in range(total_unique):
-            for j in range(total_unique):
-                for k in range(total_unique):
-                    keys = [i, j, k]
-                    print(keys)
-                    if i != j and j != k and k != i:
-                        if hashmap[i] == 0:
-                            continue
-                        else:
-                            num = "".join(str(hashmap[key]) for key in keys)
-                            if int(num) % 2 == 0:
-                                print(num)
-                                final.add(num)
-                                
-        return len(final)
+        for i, j, k in permutations(digits, 3):
+            if i != 0 and k % 2 == 0:
+                result.add((i, j, k))
+            
+        return len(result)
         
